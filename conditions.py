4a6c273abd592b0a8c68a1535615078f5ac8848f
Srilatha <koduri0311@gmail.com>
2024-12-17 19:12:35 +0000

    conditions

---
+++ conditions.py
@@ -0,0 +1,18 @@
+# if, elseif, else
+# num1 = 500
+
+# if (num1 > 50) and (num1 <= 100):
+#     print("Number is less than or equal to 100 and greater than 50")
+# elif num1 <= 50: # elif: else if
+#     print("Number is less than or equal to 50")
+# else:
+#     print("Number is greater than 100")
+
+# if True:
+#     print("I am evaluated")
+
+# Anything that is non-zero is considered as true
+if "false":
+    print("I am evaluated")
+else:
+    print("Zero")
